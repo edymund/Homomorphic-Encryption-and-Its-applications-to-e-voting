211901091838767d5ff66eead3fb44664b19acbd
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2021-12-14 22:25:25 +0800

    Skeleton for User Pages

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,19 @@
 from .boundary.landingPageBoundary import landingPageBoundary
+from .boundary.user_editProfileBoundary import user_editProfileBoundary
 from app import application as app, boundary, loginRequired
 from flask import request
 
 @app.route('/', methods=['GET'])
 def landingPage():
-	# Creates a controller object
+	# Creates a boundary object
 	boundary = landingPageBoundary()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+@app.route('/edit_profile', methods=['GET'])
+def editProfilePage():
+	# Create a boundary object
+	boundary = user_editProfileBoundary()
 	if request.method == 'GET':
 		return boundary.displayPage()
 
