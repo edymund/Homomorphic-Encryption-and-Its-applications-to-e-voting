ce5d4c8a4ccc1e902be996ffb08103ad9b72fd18
Thunderbolt <thunderbolt202105@gmail.com>
2021-12-20 21:43:23 +0800

    view election message, view email setting, view import voter list,
    css,route,html,boundary

---
+++ app/boundary/user_viewElectionMessage.py
@@ -0,0 +1,9 @@
+from flask import render_template
+class user_viewElectionMessageBoundary:
+	# Constructor
+	def __init__(self):
+		pass
+
+	# Other Methods
+	def displayPage(self):
+		return render_template('user_viewElectionMessage.html')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,10 @@
+from app.boundary.user_viewElectionMessage import user_viewElectionMessageBoundary
 from .boundary.landingPageBoundary import landingPageBoundary
 from .boundary.user_editProfileBoundary import user_editProfileBoundary
+from .boundary.user_viewElectionMessage import user_viewElectionMessageBoundary
+from .boundary.user_viewImportVoterList import user_viewImportVoterListBoundary
+from .boundary.user_viewEmailSetting import user_viewEmailSettingsBoundary
+
 from app import application as app, boundary, loginRequired
 from flask import request
 
@@ -17,6 +22,27 @@ def editProfilePage():
 	if request.method == 'GET':
 		return boundary.displayPage()
 
+@app.route('/view_electionMessage', methods=['GET'])
+def view_electionMessage():
+	# Create a boundary object
+	boundary = user_viewElectionMessageBoundary()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+@app.route('/view_importList', methods=['GET'])
+def view_importList():
+	# Create a boundary object
+	boundary = user_viewImportVoterListBoundary()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+@app.route('/view_emailSettings', methods=['GET'])
+def view_emailSetting():
+	# Create a boundary object
+	boundary = user_viewEmailSettingsBoundary()
+	if request.method == 'GET':
+		return boundary.displayPage()
+		
 	# # Create PublicUser_ExposureStatusBoundary Object
 	# publicUser_exposureStatusBoundary = PublicUser_ExposureStatusUI()
 
