cf67139a71bfe93a72be05e4270eb0b8a86706ad
dannbumm <danielchuazh93@gmail.com>
2021-12-25 17:24:12 +0800

    main ballot page

--- app/boundary/user_mainBallotBoundary.py
+++ app/boundary/user_mainBallotBoundary.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import render_template, session, flash, redirect
+from ..controllers.user_mainBallotController import user_mainBallotController
+
 class user_mainBallotBoundary:
 	# Constructor
 	def __init__(self):
@@ -6,4 +8,7 @@ class user_mainBallotBoundary:
 
 	# Other Methods
 	def displayPage(self):
-		return render_template('user_mainBallot.html')
+		controller = user_mainBallotController()
+		user_id = session['userID']
+		data = controller.getProject(user_id)
+		return render_template('user_mainBallot.html',data=data)
