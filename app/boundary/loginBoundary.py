07a6bfc97057e98744da616b8089d0450dd8fd5e
dannbumm <danielchuazh93@gmail.com>
2021-12-25 15:10:09 +0800

    Fixed Incorrect Password

--- app/boundary/loginBoundary.py
+++ app/boundary/loginBoundary.py
@@ -37,7 +37,7 @@ class loginBoundary:
 		flash("Login successfully", 'message')
 		return redirect('/mainballot')
 	
-	#display error
+	#display error and flash message
 	def displayError(self, message):
 		flash(message, 'error')
 		return redirect('/login')
