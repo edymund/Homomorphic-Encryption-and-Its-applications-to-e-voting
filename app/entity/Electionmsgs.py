e1109c1135c7e0e5d08fc98ffc011af0b5b3edde
AnnoyingBerry <56334540+AnnoyingBerry@users.noreply.github.com>
2021-12-28 19:57:29 +0800

    added comments to routes(voters), Electionmsg entity

---
+++ app/entity/Electionmsgs.py
@@ -0,0 +1,46 @@
+from ..dbConfig import dbConnect, dbDisconnect
+
+class Electionmsgs:
+	# Constructor for user
+	def __init__(self, projID = 1):
+		# Connect to database
+		connection = dbConnect()
+		db = connection.cursor()
+		# If the projID  is provided, fill the object with details from database
+		hasResult = False
+		if projID  is not None:
+			# Select election messages from database and populate instance variables
+			result = db.execute("""SELECT  electionMsgsID, projID, preMsg, postMsg, inviteMsg, reminderMsg
+								FROM electionmsgs
+								WHERE projID = (?)""", (projID ,)).fetchone()
+
+			# If a result is returned, populate object with data
+			if result is not None:
+				hasResult = True
+				# Initialise instance variables for this object
+				self.__electionMsgsID = result[0]
+				self.__projID = result[1]
+				self.__preMsg = result[2]
+				self.__postMsg = result[3]
+				self.__inviteMsg = result[4]
+				self.__reminderMsg = result[5]
+		
+		if not hasResult:
+				self.__electionMsgsID = None
+				self.__projID = None
+				self.__preMsg = None
+				self.__postMsg = None
+				self.__inviteMsg = None
+				self.__reminderMsg = None
+
+		dbDisconnect(connection)
+	
+	# Accessor method
+
+	def preElectionMsg(self):
+		"""retrieve pre-election message"""
+		return self.__preMsg
+	
+	def postElectionMsg(self):
+		"""retrieve Post-election message"""
+		return self.__postMsg
--- app/routes.py
+++ app/routes.py
@@ -37,34 +37,6 @@ def editProfilePage():
 	if request.method == 'GET':
 		return boundary.displayPage()
 
-@app.route('/ViewVoterCoverPage', methods=['GET'])
-def viewVoterCoverPage():
-	# Create a boundary object
-	boundary = voters_ViewVoterCoverPage()
-	if request.method == 'GET':
-		return boundary.displayPage()
-
-@app.route('/ViewVotingPage', methods=['GET'])
-def viewVotingPage():
-	# Create a boundary object
-	boundary = voters_ViewVotingPage()
-	if request.method == 'GET':
-		return boundary.displayPage()
-
-@app.route('/ViewSubmittedVotePage', methods=['GET'])
-def viewSubmittedVotePage():
-	# Create a boundary object
-	boundary = voters_ViewSubmittedVotePage()
-	if request.method == 'GET':
-		return boundary.displayPage()
-
-@app.route('/ViewEncryptedVotePage', methods=['GET'])
-def viewEncryptedVotePage():
-	# Create a boundary object
-	boundary = voters_ViewEncryptedVotePage()
-	if request.method == 'GET':
-		return boundary.displayPage()
-
 @app.route('/overview', methods = ['GET'])
 def projectOverviewPage():
 	# Create a boundary object
@@ -199,6 +171,40 @@ def mainBallotPage():
 	boundary = user_mainBallotBoundary()
 	if request.method == 'GET':
 		return boundary.displayPage()
+
+###############################################
+#				Voting Pages				  #
+###############################################
+
+@app.route('/ViewVoterCoverPage', methods=['GET'])
+def viewVoterCoverPage():
+	# Create a boundary object
+	boundary = voters_ViewVoterCoverPage()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+@app.route('/ViewVotingPage', methods=['GET'])
+def viewVotingPage():
+	# Create a boundary object
+	boundary = voters_ViewVotingPage()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+@app.route('/ViewSubmittedVotePage', methods=['GET'])
+def viewSubmittedVotePage():
+	# Create a boundary object
+	boundary = voters_ViewSubmittedVotePage()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+@app.route('/ViewEncryptedVotePage', methods=['GET'])
+def viewEncryptedVotePage():
+	# Create a boundary object
+	boundary = voters_ViewEncryptedVotePage()
+	if request.method == 'GET':
+		return boundary.displayPage()
+
+###############################################
 
 	# # Create PublicUser_ExposureStatusBoundary Object
 	# publicUser_exposureStatusBoundary = PublicUser_ExposureStatusUI()
