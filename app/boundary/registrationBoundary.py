bdd58adb6e71c81722d81d9a2461df701be49946
dannbumm <danielchuazh93@gmail.com>
2021-12-29 17:26:38 +0800

    Update Validation in Backend

--- app/boundary/registrationBoundary.py
+++ app/boundary/registrationBoundary.py
@@ -1,30 +1,54 @@
 from sqlite3.dbapi2 import IntegrityError
 from flask import render_template, session, redirect, request, flash
 from ..controllers.registrationController import registrationController
+import re
 
 class registrationBoundary:
 	# Constructor
 	def __init__(self):
 		self.RESULT_FAILURE_DUPLICATE_VALUE = "{}: {} already exists"
 		self.RESULT_SUCCESS = "Success"
-		self.PASSWORD_INVALID = "Passwords does not match"
 
 	# Other Methods
 	def displayPage(self):
 		return render_template('registration.html')
 
-	
-	def onSubmit(self,email,password,cfm_password,companyName,firstName,lastName):
-		controller = registrationController()
+	def __checkIsValidPassword(self, password, cfm_password):
+		# Check if passwords match
 		if password != cfm_password:
-			return self.PASSWORD_INVALID
-		else:
+			self.ERROR = "Password fields do not match"
+			return False
+		return True
+
+	def __checkIsValidFullName(self, firstName, lastName):
+		# Check first name, last name
+		if not re.search('^[a-zA-Z]+$', firstName) or \
+			not re.search('^[a-zA-Z]+$', lastName):
+			self.ERROR = "Name contain invalid characters"
+			return False
+		return True
+
+	def __checkEmailFormat(self,email):
+		# Check first name, last name
+		if email != "":
+			if not re.search('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$', email):
+				self.ERROR = "Email format is not valid"
+				return False
+		return True
+
+	def onSubmit(self,email,password,cfm_password,companyName,firstName,lastName):
+		if self.__checkIsValidFullName(firstName, lastName) and \
+			self.__checkIsValidPassword(password, cfm_password) and \
+			self.__checkEmailFormat(email):
 			try:
-				controller.addUser(email,password,companyName,firstName,lastName)
+				controller = registrationController
+				controller.addUser(self,email,password,companyName,firstName,lastName)
 				return self.RESULT_SUCCESS
 			except IntegrityError:
 				return self.RESULT_FAILURE_DUPLICATE_VALUE.format('Email', email)
-
+		else:
+			return self.ERROR
+	
 	
 	def displaySuccess(self):
 		flash("Account created successfully", 'message')
--- app/boundary/user_settingsBoundary.py
+++ app/boundary/user_settingsBoundary.py
@@ -1,7 +1,7 @@
 from sqlite3.dbapi2 import IntegrityError
 from flask import render_template, session, flash, redirect
 from ..controllers.user_settingsController import user_settingsController
-import sys
+import re
 
 class user_settingsBoundary:
 	# Constructor
@@ -14,18 +14,44 @@ class user_settingsBoundary:
 	def displayPage(self):
 		return render_template('user_settings.html')
 
+	def __checkIsValidFirstName(self, first_name):
+		# Check first name, last name
+		if first_name != "":
+			if not re.search('^[a-zA-Z]+$', first_name):
+				self.ERROR = "Name contain invalid characters"
+				return False
+		return True
+
+	def __checkIsValidLastName(self,last_name):
+		# Check first name, last name
+		if last_name != "":
+			if not re.search('^[a-zA-Z]+$', last_name):
+				self.ERROR = "Name contain invalid characters"
+				return False
+		return True
+
+	def __checkEmailFormat(self,email):
+		# Check first name, last name
+		if email != "":
+			if not re.search('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$', email):
+				self.ERROR = "Email format is not valid"
+				return False
+		return True
 
 	def onSubmit(self,first_name,last_name,email,company_name):
-		username = session['user']
+		if self.__checkIsValidFirstName(first_name) and \
+			self.__checkIsValidLastName(last_name) and \
+			self.__checkEmailFormat(email):
+			try:
+				username = session['user']
+				controller = user_settingsController
+				controller.updateUserDetails(self,username,first_name,last_name,email,company_name)
+				return self.RESPONSE_SUCCESS
+			except IntegrityError:
+				return self.RESULT_FAILURE_DUPLICATE_VALUE.format('Email', email)
+		else:
+			return self.ERROR
 
-		controller = user_settingsController()
-
-		# Calls the controller to update the user's details
-		try:
-			controller.updateUserDetails(username,first_name,last_name,email,company_name)
-			return self.RESPONSE_SUCCESS
-		except IntegrityError:
-			return self.RESULT_FAILURE_DUPLICATE_VALUE.format('Email', email)
 
 	def displaySuccess(self):
 		flash("Changed Details Successfully", 'message')
