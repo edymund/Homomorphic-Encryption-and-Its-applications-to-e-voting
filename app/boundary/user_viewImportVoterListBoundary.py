db460aae54c1dcc2c64c4479a499209120ee36ad
Thunderbolt <thunderbolt202105@gmail.com>
2022-01-06 18:47:44 +0800

    deleted-static method

--- app/boundary/user_viewImportVoterListBoundary.py
+++ app/boundary/user_viewImportVoterListBoundary.py
@@ -30,13 +30,3 @@ class user_viewImportVoterListBoundary:
 		# controller.setProjID()
 		voters_list = controller.get_all_voters_email()
 		return voters_list
-
-	@staticmethod
-	def retrieve_proj_detail(url):
-		# url = "www.123/1/abc"
-		# for i in range(1):
-		slash = url.find("/view_importList")
-		new_url = url[:slash]
-		next_slash = new_url.rfind("/")
-		proj_details = new_url[next_slash+1:]
-		return proj_details
