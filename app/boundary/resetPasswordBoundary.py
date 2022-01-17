ef0cfeb760a378851fd862a296d2cd25c579e29c
dannbumm <danielchuazh93@gmail.com>
2022-01-17 11:51:04 +0800

    update send email into a function

--- app/boundary/resetPasswordBoundary.py
+++ app/boundary/resetPasswordBoundary.py
@@ -3,6 +3,7 @@ from flask import render_template, session, redirect, request, flash
 from ..controllers.resetPasswordController import resetPasswordController
 import random
 import smtplib
+import string
 
 class resetPasswordBoundary:
 	# Constructor
@@ -13,21 +14,35 @@ class resetPasswordBoundary:
 	# Other Methods
 	def displayPage(self):
 		return render_template('resetPassword.html')
+
+	def random_pw(self,no_of_letters,no_of_num):
+		letters = ''.join((random.choice(string.ascii_letters) for i in range(no_of_letters)))
+		digits = ''.join((random.choice(string.digits) for i in range(no_of_num)))
+		# Convert resultant string to list and shuffle it to mix letters and digits
+		random_list = list(letters + digits)
+		random.shuffle(random_list)
+		# convert list to string
+		new_random_password = ''.join(random_list)
+		return new_random_password	
 	
+	def send_reset_pw(self,new_password):
+		email_address = "fyp21s403@gmail.com"
+		server = smtplib.SMTP("smtp.gmail.com",587)
+		server.starttls()
+		# login(email address, password)
+		server.login(email_address, "eccqringtcgtolnf")
+		msg = 'From: ' + email_address + '\r\nTo: ' + email_address + '\r\nSubject: Reset Password \r\n\r\nYour Password has been reset to: ' + str(new_password)
+		# sendmail(sender,receiver,message)
+		server.sendmail(email_address,email_address,msg)
+
 	def onSubmit(self,email):
 		controller = resetPasswordController()
 		if controller.checkifEmailexists(email) == True:
-			new_password = random.randint(100, 999) 
+			# call random password method with num of letters and digits
+			new_password = self.random_pw(5,3) 
 			controller.resetPw(email,new_password)
-			email_address = "fyp21s403@gmail.com"
-			server = smtplib.SMTP("smtp.gmail.com",587)
-			server.starttls()
-			# login(email address, password)
-			server.login(email_address, "eccqringtcgtolnf")
-			msg = 'From: ' + email_address + '\r\nTo: ' + email_address + '\r\nSubject: Reset Password \r\n\r\nYour Password has been reset to: ' + str(new_password)
-			# sendmail(sender,receiver,message)
-			server.sendmail(email_address,email_address,msg)
-
+			# send random password to user via email
+			self.send_reset_pw(new_password)
 			return self.RESPONSE_SUCCESS
 		else:
 			return self.RESPONSE_INCORRECT_EMAIL
