0cfbd7bd3310108ef3ff435291bb1cfe9c1de737
LemonzTea <54799314+LemonzTea@users.noreply.github.com>
2022-02-12 17:01:49 +0800

    Updated Page to Generate and Dowload Keys

--- app/routes.py
+++ app/routes.py
@@ -422,17 +422,5 @@ def logout():
 def generateKeysPage():
 	# Creates a boundary object
 	boundary = generateKeysBoundary()
-	if request.method == 'GET':
-		return boundary.displayPage()
-	if request.method == 'POST':
-		return boundary.displayPage()
-	# # Create PublicUser_ExposureStatusBoundary Object
-	# publicUser_exposureStatusBoundary = PublicUser_ExposureStatusUI()
 
-	# # Exposure status is none if user is not a public user
-	# exposureStatus = publicUser_exposureStatusBoundary.getExposureStatus()
-
-	# # Displays the webpage
-	# return render_template('overview.html', userType = session['userType'],
-	# 										healthStatus = exposureStatus)
-
+	return boundary.displayPage()
