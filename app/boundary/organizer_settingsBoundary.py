2bbd6c5ba924c5768998e1cfeca1e1c5da7cf894
dannbumm <danielchuazh93@gmail.com>
2022-02-15 11:45:29 +0800

    update session in change password and settings to 'user' instead of organizer

--- app/boundary/organizer_settingsBoundary.py
+++ app/boundary/organizer_settingsBoundary.py
@@ -43,7 +43,7 @@ class organizer_settingsBoundary:
 			self.__checkIsValidLastName(last_name) and \
 			self.__checkEmailFormat(email):
 			try:
-				username = session['organizer']
+				username = session['user']
 				controller = organizer_settingsController
 				controller.updateUserDetails(self,username,first_name,last_name,email,company_name)
 				return self.RESPONSE_SUCCESS
