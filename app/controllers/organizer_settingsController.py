687c42c11c933ec0f9d89c90a85b8886b18cb866
dannbumm <danielchuazh93@gmail.com>
2022-02-17 14:00:09 +0800

    Bugs fixed: settings now show current settings and cannot be updated when fields are blank

--- app/controllers/organizer_settingsController.py
+++ app/controllers/organizer_settingsController.py
@@ -5,6 +5,10 @@ class organizer_settingsController:
 	def __init__(self):
 		pass
 
+	def getUserDetails(self, username):
+		getDetails = Organizers(username)
+		return getDetails.getUserDetails(username)
+
 	def updateUserDetails(self,username,first_name,last_name,email,company_name):
 
 		# Create a user object containing details of the owner
