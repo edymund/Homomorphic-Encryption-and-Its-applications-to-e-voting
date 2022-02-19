11e7503c793a0a0746cb88801f1d2345a8a92796
Thunderbolt <thunderbolt202105@gmail.com>
2022-02-19 13:31:24 +0800

    Gixed bug

--- app/controllers/organizer_ElectionMsgController.py
+++ app/controllers/organizer_ElectionMsgController.py
@@ -8,40 +8,27 @@ class ElectionMsgController:
     def getProjID(self):
         return self.projectID
 
-    def getPreMsg(self):
-        return self.preMsg
-
-    def getPostMsg(self):
-        return self.postMsg
-
-    # mutator 
-    def setProjID(self,projectID):
-        self.projID = projectID
-
     def check_election_msg(self,msg):
         if msg == "" or msg ==None:
             return False
         elif msg != "" and msg != None:
             return True
 
-    def update_pre_election_msg(self,msg):
-        entity = ElectionMessage(projID= self.projectID)
-        entity.setPreMsg(msg)
+    def update_pre_election_msg(self,msg,projectID):
+        entity = ElectionMessage(projectID)
+        entity.setPreMsg(msg,projectID)
     
-    def update_post_election_msg(self,msg):
-        entity = ElectionMessage(projID= self.projectID)
-        entity.setPostMsg(msg)
+    def update_post_election_msg(self,msg,projectID):
+        entity = ElectionMessage(projectID)
+        entity.setPostMsg(msg,projectID)
 
-    def retrieve_pre_election_msg(self):
-        entity = ElectionMessage(projID= self.projectID)
-        self.preMsg = entity.getPreMsg()
-        return self.preMsg
+    def retrieve_pre_election_msg(self,projectID):
+        entity = ElectionMessage(projectID)
+        return entity.getPreMsg()
 
-    def retrieve_post_election_msg(self):
-        entity = ElectionMessage(projID= self.projectID)
-        self.postMsg = entity.getPostMsg()
-        return self.postMsg
-
+    def retrieve_post_election_msg(self,projectID):
+        entity = ElectionMessage(projectID)
+        return entity.getPostMsg()
     
     
     
