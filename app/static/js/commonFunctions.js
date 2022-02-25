f03ef23453c759b5139461128392d83602c79d21
Thunderbolt <thunderbolt202105@gmail.com>
2022-02-25 23:21:53 +0800

    changes

--- app/static/js/commonFunctions.js
+++ app/static/js/commonFunctions.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
 
     $(window).keydown(function(event){
 
-        if((event.keyCode == 13) && ($(event.target)[0]!=$("textarea")[0])) {
+        if((event.keyCode == 13) && ($(event.target)[0]!=$("textarea")[0]&& ($(event.target)[0]!=$("textarea")[1]))) 
+    {
 
             event.preventDefault();
 
